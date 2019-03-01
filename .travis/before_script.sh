310f31d25307e4f0b3591c74881678be7957fdbc
Austin Macdonald <austin@redhat.com>
2019-03-01 13:31:06 -0500

    Try letting travis install postgres

--- .travis/before_script.sh
+++ .travis/before_script.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 set -v
 
-# psql -U postgres -c 'CREATE USER pulp WITH SUPERUSER LOGIN;'
-# psql -U postgres -c 'CREATE DATABASE pulp3 OWNER pulp;'
-#
-# mkdir -p ~/.config/pulp_smash
-# cp .travis/pulp-smash-config.json ~/.config/pulp_smash/settings.json
-#
+psql -U postgres -c 'CREATE USER pulp WITH SUPERUSER LOGIN;'
+psql -U postgres -c 'CREATE DATABASE pulp3 OWNER pulp;'
+
+mkdir -p ~/.config/pulp_smash
+cp .travis/pulp-smash-config.json ~/.config/pulp_smash/settings.json
+
 # sudo mkdir -p /var/lib/pulp/tmp
 # sudo mkdir /etc/pulp/
 # sudo chown -R travis:travis /var/lib/pulp
